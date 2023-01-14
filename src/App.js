dcce7f1cba712351edec60126ab7bbe21472101e
Sohel <Sohel@sayyedsohel999@gmail.com>
2023-01-14 23:43:30 +0530

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import axios from 'axios'
+import ShowTemp from "./ShowTemp";
+import "./App.css"
 
 function App() {
+
+  const[city,setCity] = useState("");
+  const[data,setData] = useState({
+     description:"",
+     temp:0,
+     temp_max:0,
+     temp_min:0,
+     humidity:0,
+     sunrise:0,
+     sunset:0,
+     country:"",
+  })
+
+  const handleClick =()=>{
+     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=69b2be9dd1d32f349de7bcea8e33ee1f`)
+     .then((response)=>{
+      setData({
+        description:response.data.weather[0].description,
+        temp:response.data.main.temp,
+        temp_max:response.data.main.temp_max,
+        temp_min:response.data.main.temp_min,
+        humidity:response.data.main.humidity,
+        sunrise:response.data.sys.sunrise,
+        sunset:response.data.sys.sunset,
+        country:response.data.sys.country,
+      })
+     })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className="container text-center my-2">
+          <h1>Weather-App</h1>
+          <input type="text" className="form-control" value={city} onChange={(e)=>{
+            setCity(e.target.value)
+          }} />
+          <button className="btn btn-primary mx-2 mt-4" type="submit" onClick={handleClick}>Get Temp</button>
+       </div>
+        <ShowTemp text={data}></ShowTemp>
+      </>
   );
 }
 
